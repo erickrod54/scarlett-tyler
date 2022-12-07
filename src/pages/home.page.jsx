e9745732b5f812086a1e79bca168deafc7996bbf
Erick Rodriguez <erickrod54@gmail.com>
2022-12-07 14:24:40 -0500

    Scarlett-taylor Portfolio - version 1.01 - HomePage - Features: Commenting Components to start working in the SocialSidebar

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { Aboutme, Contact, Hero, Navbar, Skills, SocialSidebar, Testimonials, Work } from "../components/index.components";
 
+/*Scarlett-taylor Portfolio - version 1.01 - 
+*HomePage - Features: 
+*  
+*     --> Commenting Components to 
+*         start working in the SocialSidebar.
+*
+* Note: this file will be refactor
+*/
 
 
 const HomePage = () => {
 
     return(
         <>
-            <Navbar />
-            <Hero />
+            {/**<Navbar />
+            <Hero /> */}
             <SocialSidebar />
-            <Work />
+            {/** <Work />
             <Aboutme />
             <Skills />
             <Testimonials />
-            <Contact />    
+            <Contact />*/}    
         </>
     )
 }
