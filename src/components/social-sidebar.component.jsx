dd0e5b2700ebac901edf5ca99f5d8fc3b754a5cf
Erick Rodriguez <erickrod54@gmail.com>
2022-12-07 14:17:22 -0500

    Scarlett-Taylor Portfolio - version 1.01 - SocialSidebar -Features: Starting to build SocialSidebar, Destructuring icons from the context

--- src/components/social-sidebar.component.jsx
+++ src/components/social-sidebar.component.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+import { Logo } from "../assets/assets.index";
+import { useAppContext } from "../context";
+
+/*Scarlett-taylor Portfolio - version 1.01 - 
+* SocialSidebar - Features: 
+*  
+*     --> Starting to build 'SocialSidebar'
+*
+*     --> Destructuring icons from the context.
+*  
+* Note: this file will be refactor
+*/
 
 const SocialSidebar = () => {
-
+    
+    const { facebook, instagram, twitter, linkedin, dribble, pinterest } = useAppContext()
+    console.log('data from context ==>', facebook, instagram, twitter, linkedin, dribble, pinterest)
+    
     return(
-        <h2>SocialSidebar Component</h2>
+        <Wrapper>
+            <aside className="side-bar">
+                <div className="side-nav">
+                    <div className="side-nav__content">
+                        <div className="side-nav__content--logo">
+                            <img src={Logo} alt='logo'/>
+                        </div>
+                        <ul className='side-nav__content--social'>
+                           
+                        </ul>        
+                    </div>
+                </div>
+            </aside>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    img{
+        width: 150px;
+        height: 200px;
+    }
+`
 
 export default SocialSidebar;
