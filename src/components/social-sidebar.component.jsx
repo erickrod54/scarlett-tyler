9f8b032695ba32ca6cc754876ecddde0ca7f0a8a
Erick Rodriguez <erickrod54@gmail.com>
2022-12-08 12:29:09 -0500

    Scarlett-taylor Portfolio - version 1.02 - SocialSidebar - Features: Building SocialSidebar, Building and styling side-nav__content--social

--- src/components/social-sidebar.component.jsx
+++ src/components/social-sidebar.component.jsx
@@ -3,14 +3,17 @@ import styled from "styled-components";
 import { Logo } from "../assets/assets.index";
 import { useAppContext } from "../context";
 
-/*Scarlett-taylor Portfolio - version 1.01 - 
+/**
+* Scarlett-taylor Portfolio - version 1.02 - 
 * SocialSidebar - Features: 
 *  
-*     --> Starting to build 'SocialSidebar'
+*     --> Building 'SocialSidebar'
 *
-*     --> Destructuring icons from the context.
+*     --> Building and styling 
+*          'side-nav__content--social'  
 *  
-* Note: this file will be refactor
+* Note: pending to convert 'li' in 'links' when
+* react router-dom will implemented.
 */
 
 const SocialSidebar = () => {
@@ -27,7 +30,24 @@ const SocialSidebar = () => {
                             <img src={Logo} alt='logo'/>
                         </div>
                         <ul className='side-nav__content--social'>
-                           
+                            <li>
+                             {facebook}      
+                            </li>
+                            <li>
+                             {instagram}   
+                            </li>
+                            <li>
+                              {twitter}  
+                            </li>
+                            <li>
+                              {linkedin}  
+                            </li>
+                            <li>
+                              {dribble}                             
+                            </li>
+                            <li>
+                                {pinterest}                             
+                            </li>
                         </ul>        
                     </div>
                 </div>
@@ -37,9 +57,55 @@ const SocialSidebar = () => {
 }
 
 const Wrapper = styled.div`
-    img{
-        width: 150px;
-        height: 200px;
+    .side-nav__content--logo img{
+        width: calc(7vw + 5vh);
+        object-fit: cover;
+    }
+
+    .side-nav{
+        position: sticky;
+        top: 0;
+        left: 0;
+        height: 100vh;
+    }
+
+    .side-nav__content{
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        background-color: var(--primary-color);
+    }
+
+    .side-nav__content--social{
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-end;
+
+        li{
+            list-style-type: none;
+            /**20px => 2rem => (1rem + [10/1500] + 100 -0.666667-)
+            * Dynamic value for an aspect ratio oof 1500px */
+            font-size: calc(1rem + 0.666667vw);
+            color: white;
+
+            /***make the transition smooth */
+            transition: color 0.35s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+            padding-bottom: calc(2rem + 0.666667vw);
+        }
+
+        /**pending to fix hover effect ( waiting for 
+            router implementation ) */
+        li:hover
+        li:active{
+           color:var(--secondary-color) ;
+        }
+
     }
 `
 
