117f3fe16e265c9c4031c0155ed9eac17fe828eb
Erick Rodriguez <erickrod54@gmail.com>
2022-12-08 12:24:19 -0500

    Scarlett-taylor portfolio - version 1.02 - data js - Features: Importing free-brands icons from font awesome to build the SocialSidebar

---
+++ src/data.js
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faTwitter, faLinkedin, faDribbble, faPinterest } from '@fortawesome/free-brands-svg-icons'
+
+/**
+ * Scarlett-taylor portfolio - version 1.02 - data js -
+ * Features:
+ *
+ *      --->Importing 'free-brands' icons from font awesome
+ *          to build the 'SocialSidebar'  
+ * 
+ * Note: this file is going to be added more data
+ * 
+ * * */
+export const facebook = <FontAwesomeIcon icon={faFacebook} />
+export const instagram = <FontAwesomeIcon icon={faInstagram} />
+export const twitter = <FontAwesomeIcon icon={faTwitter} />
+export const linkedin = <FontAwesomeIcon icon={faLinkedin} />
+export const dribble = <FontAwesomeIcon icon={faDribbble} />
+export const pinterest = <FontAwesomeIcon icon={faPinterest} />
