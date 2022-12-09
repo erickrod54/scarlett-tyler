284c2bf817667bd633550b717efd9ff002513547
Erick Rodriguez <erickrod54@gmail.com>
2022-12-09 14:21:21 -0500

    Scarlett-taylor Portfolio - version 1.03 - HomePage - Features: Uncommenting Navbar to start to work on it

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Aboutme, Contact, Hero, Navbar, Skills, SocialSidebar, Testimonials, Work } from "../components/index.components";
 
-/*Scarlett-taylor Portfolio - version 1.01 - 
+/*Scarlett-taylor Portfolio - version 1.03 - 
 *HomePage - Features: 
 *  
-*     --> Commenting Components to 
-*         start working in the SocialSidebar.
+*     --> Uncommenting 'Navbar' to start to 
+*         work on it.
 *
 * Note: this file will be refactor
 */
@@ -15,8 +15,8 @@ const HomePage = () => {
 
     return(
         <>
-            {/**<Navbar />
-            <Hero /> */}
+            <Navbar />
+           {/** <Hero /> */}
             <SocialSidebar />
             {/** <Work />
             <Aboutme />
