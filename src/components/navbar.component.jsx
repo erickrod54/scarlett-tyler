590dfa1d82067d2610af39e1c846557eabeadb47
Erick Rodriguez <erickrod54@gmail.com>
2022-12-09 14:16:30 -0500

    Scarlett-taylor portfolio - version 1.03 - Navbar -Features: Starting to build Navbar

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import { useAppContext } from "../context";
+
+ /**
+ * Scarlett-taylor portfolio - version 1.03 - Navbar -
+ * Features:
+ *
+ *      ---> Starting to build 'Navbar'.  
+ * 
+ * Note: this file is going to be added more data
+ * 
+ * * */
 
 const Navbar = () => {
+    const { AppLinks } = useAppContext()
 
+    console.log('data applinks =>', AppLinks)
     return(
-        <h2>Navbar Component</h2>
+        <Wrapper>
+            <h2>Navbar Component</h2>
+
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+
+`
 
 export default Navbar;
