b9ed59677060645cf369cd71f1a2b80051e422c7
Erick Rodriguez <erickrod54@gmail.com>
2022-12-09 14:14:58 -0500

    Scarlett-taylor portfolio - version 1.03 - App js - Features: Implementing react-router-dom for navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import HomePage from "./pages/home.page";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { About, ContactPage, HireMe, HomePage, ProjectsPage, ServicesPage } from "./pages/index-pages";
+
+ /**
+ * Scarlett-taylor portfolio - version 1.03 - App js -
+ * Features:
+ *
+ *      ---> Implementing 'react-router-dom' 
+ *            for navigation.  
+ * 
+ * Note: this file is going to be added more data
+ * 
+ * * */
 
 function App() {
   return (
-    <HomePage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route path="about" element={<About />}/>
+          <Route path="projects" element={<ProjectsPage />}/>
+          <Route path="services" element={<ServicesPage />}/>
+          <Route path="hire-me" element={<HireMe />}/>
+          <Route path="contact" element={<ContactPage />}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
