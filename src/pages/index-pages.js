813b488eab6869d820d69bc77ee6888ab81c7aa6
Erick Rodriguez <erickrod54@gmail.com>
2022-12-10 22:44:51 -0500

    Scarlett-taylor portfolio - version 1.04 - pages > index - Features: Adding pages to the pages > index

---
+++ src/pages/index-pages.js
@@ -0,0 +1,19 @@
+import About from './about.page';
+import ContactPage from './contact.page';
+import HireMe from './hire.me.page';
+import HomePage from './home.page';
+import ProjectsPage from './projects.page';
+import ServicesPage from './services.page';
+
+ /**
+ * Scarlett-taylor portfolio - version 1.04 - pages 
+ * > index - Features:
+ *
+ *      ---> Adding pages to the pages > index.  
+ * 
+ * Note: this file is going to be added more data
+ * 
+ * * */
+
+
+export { About, ContactPage, HireMe, HomePage, ProjectsPage, ServicesPage } 
