008fb0ac4cfcdb57c5707f93e47c2eb5f3880c2e
Erick Rodriguez <erickrod54@gmail.com>
2022-12-10 22:42:33 -0500

    Scarlett-taylor Portfolio - version 1.04 - assets.index - Features: Adding new logo

---
+++ src/assets/assets.index.js
@@ -0,0 +1,12 @@
+import Logo from './logo.jpg'
+
+/*Scarlett-taylor Portfolio - version 1.04 - 
+* assets.index - Features: 
+*  
+*     --> Adding new logo.
+*  
+* Note: this file will be refactor
+*/
+
+
+export { Logo } 
