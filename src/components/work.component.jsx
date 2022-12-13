ae8fdd8f0aeef38bdca147d253f9a7e92bd0d16d
Erick Rodriguez <erickrod54@gmail.com>
2022-12-13 03:47:33 -0500

    Scarlett-taylor Portfolio - version 1.07 - assets.index - Features: Starting to build Work Component

--- src/components/work.component.jsx
+++ src/components/work.component.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+import { useAppContext } from "../context";
+
+/*Scarlett-taylor Portfolio - version 1.07 - 
+* assets.index - Features: 
+*  
+*     --> Starting to build 'Work' Component.
+*  
+* Note: this file will be refactor
+*/
 
 const Work = () => {
+    
+    const { WorkImages } = useAppContext()
 
+    console.log('assets to build work ==>', WorkImages)
     return(
-        <h2>Work Component</h2>
+        <Wrapper>
+            {WorkImages.map((picture) => {
+                const { id, workimage, name } = picture;
+
+                return(
+                <section key={id} className="work">
+                    <div className="work_card">
+                        <img src={workimage} alt={name}/>
+                    </div>
+                </section>
+
+                )
+            })}
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    .work{
+        .work_card{
+
+        }
+    }
+`
 
 export default Work;
