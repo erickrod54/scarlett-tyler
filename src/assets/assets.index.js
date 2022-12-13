e0366ba03297835d12d7da45011bf7d088ff1ab3
Erick Rodriguez <erickrod54@gmail.com>
2022-12-13 03:42:29 -0500

    Scarlett-taylor Portfolio - version 1.07 - assets.index - Features: Adding new assets for work component

--- src/assets/assets.index.js
+++ src/assets/assets.index.js
@@ -1,13 +1,19 @@
-import Logo from './logo.jpg'
-import herofullimage from './hero-full-image.jpg'
+import Logo from './logo.jpg';
+import herofullimage from './hero-full-image.jpg';
+import poster from './portfolio-branding-01-1.jpg';
+import paperbag from './portfolio-branding-02-1.jpg';
+import clothbag from './portfolio-creative-01-1.jpg';
+import branding from './portfolio-creative-02-1.jpg';
+import cup from './portfolio-design-01-1.jpg';
+import printer from './portfolio-print-02-1.jpg';
 
-/*Scarlett-taylor Portfolio - version 1.06 - 
+/*Scarlett-taylor Portfolio - version 1.07 - 
 * assets.index - Features: 
 *  
-*     --> Adding new 'herofullimage'.
+*     --> Adding new assets 'work' component.
 *  
 * Note: this file will be refactor
 */
 
 
-export { Logo, herofullimage } 
+export { Logo, herofullimage, poster,paperbag, clothbag, branding, cup, printer } 
