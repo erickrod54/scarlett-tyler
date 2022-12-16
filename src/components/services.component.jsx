8ca36cd7dda626b00e762b7eec7ffd00590ffe94
Erick Rodriguez <erickrod54@gmail.com>
2022-12-16 10:48:36 -0500

    Scarlett-taylor Portfolio - version 1.09 - Services - Features: Starting to build Services, Destructuring and testing data from the context

--- src/components/services.component.jsx
+++ src/components/services.component.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+import { useAppContext } from "../context";
+
+/*Scarlett-taylor Portfolio - version 1.09 - 
+* Services - Features: 
+*  
+*     --> Startting to build 'Services'
+*  
+*     --> Destructuring and testing data from 
+*          the context    
+*       
+* Note: this file will be refactor
+*/
 
 const Services = () => {
+    
+    const { ServicesData } = useAppContext()
 
+    console.log('the ServicesData =>',ServicesData)
+    
     return(
-        <h2>Services Component</h2>
+        <Wrapper>
+            {ServicesData.map((service) => {
+                const { id, icon, name, description } = service;
+
+                return(
+                    <div key={id} className="service">
+                        <h3>{name}</h3>
+                        <p>{description}</p>
+                    </div>                  
+                )
+            })}
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+
+`
 
 export default Services;
