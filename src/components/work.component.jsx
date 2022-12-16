e8a20580b372f5c491482c6232207b3979c17bbc
Erick Rodriguez <erickrod54@gmail.com>
2022-12-15 23:27:50 -0500

    Scarlett-taylor Portfolio - version 1.08 - assets.index - Features: Finish Building Work Component, Finish writting styles

--- src/components/work.component.jsx
+++ src/components/work.component.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { useAppContext } from "../context";
 
-/*Scarlett-taylor Portfolio - version 1.07 - 
+/*Scarlett-taylor Portfolio - version 1.08 - 
 * assets.index - Features: 
 *  
-*     --> Starting to build 'Work' Component.
+*     --> Finish Building Work Component.
+*
+*     --> Finish writting styles.
 *  
 * Note: this file will be refactor
 */
@@ -34,11 +36,45 @@ const Work = () => {
 }
 
 const Wrapper = styled.div`
-    .work{
-        .work_card{
+        /**this is the style for the background */
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
 
+        padding: 2rem;
+        background-color: var(--tertiary-color);
+
+        /**this is the style for the cards */
+        .work_card{
+            padding: 2rem;
+            flex-basis: 40rem;
+            flex-grow: 1;
+
+            /**this is the style for the corners */
+            clip-path: polygon(
+                 20% 0%,
+                 80% 0%,
+                 100% 20%,
+                 100% 80%,
+                 80% 100%,
+                 20% 100%,
+                 0% 80%,
+                 0% 20%
+                 );
         }
-    }
+
+        /**this is the styles for the image */
+        .work_card img{
+            height: 40rem;
+            width: 40rem;
+            object-fit: cover;
+        }
+
+        .work_card img:hover,
+        .work_card img:active{
+            filter: sepia(80%);
+        }
+   
 `
 
 export default Work;
