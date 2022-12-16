bde5103ae176cfd19e2c42692d841b9143a19065
Erick Rodriguez <erickrod54@gmail.com>
2022-12-16 10:45:07 -0500

    Scarlett-taylor Portfolio - version 1.09 - Aboutme - Features: Finish Building Aboutme Component, Destructuring data from the context

--- src/components/about.me.component.jsx
+++ src/components/about.me.component.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useAppContext } from "../context";
 
-/*Scarlett-taylor Portfolio - version 1.08 - 
+/*Scarlett-taylor Portfolio - version 1.09 - 
 * Aboutme - Features: 
 *  
-*     --> Start Building 'Aboutme' Component.
+*     --> Finish Building 'Aboutme' Component.
 *
 *     --> Destructuring data from the context  
 *         
@@ -16,9 +16,9 @@ import { useAppContext } from "../context";
 const Aboutme = () => {
 
     const { Aboutimg } = useAppContext()
-    console.log('Aboutimg', Aboutimg)
-    console.log('Aboutimg is ==>', Aboutimg[0].aboutimg)
-    console.log('Aboutimg is ==>', Aboutimg[0].name)
+    //console.log('Aboutimg', Aboutimg)
+    //console.log('Aboutimg is ==>', Aboutimg[0].aboutimg)
+    //console.log('Aboutimg is ==>', Aboutimg[0].name)
 
     return(
         <Wrapper> 
@@ -75,6 +75,11 @@ const Wrapper = styled.div`
         position: relative;
     }
 
+    a{
+        color: white;
+        text-decoration: none;
+    }
+
     .about-me_content--title::after{
         content: '';
         position: absolute;
@@ -89,6 +94,40 @@ const Wrapper = styled.div`
     .about-me_content--para{
         margin-bottom: 4rem;
         letter-spacing: 0.2rem;
+    }
+
+    /**effects for the readme link */
+    .about-me_content--link:link,
+    .about-me_content--link:visited{
+        padding: 1rem 0;
+        font-size: 1.5rem;
+        letter-spacing: 0.2rem;
+        text-transform: uppercase;
+        
+        position: relative;
+        z-index: 1;
+    }
+
+    .about-me_content--link::after,
+    .about-me_content--link::after{
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: var(--primary-color);
+        transform: scaleY(0);
+        transform-origin: bottom;
+        transition: transform 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+        
+        z-index: -1;
+    }
+
+    .about-me_content--link:hover::after,
+    .about-me_content--link:active::after{
+        transform: scale(1);
+        transform-origin: top;
     }
 `
 
