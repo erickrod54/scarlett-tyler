765745f13ae425898011c87dfc0da75e23e2d38d
Erick Rodriguez <erickrod54@gmail.com>
2022-12-17 01:49:29 -0500

    Scarlett-taylor portfolio - verison 2.00 - data js - Features: Building WorkImages array, Importing icons and adding them to ServicesData

--- src/data.js
+++ src/data.js
@@ -1,13 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram, faTwitter, faLinkedin, faDribbble, faPinterest } from '@fortawesome/free-brands-svg-icons'
 import { aboutimg, branding, clothbag, cup, paperbag, poster, printer } from "./assets/assets.index";
-
+import { faListCheck, faMobileScreenButton, faPaintBrush, faPaintRoller, faShareNodes } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 
 /**
- * Scarlett-taylor portfolio - version 1.08 - data js -
+ * Scarlett-taylor portfolio - version 2.00 - data js -
  * Features:
  *
  *      --->Building 'WorkImages' array
+ * 
+ *      --> Importing icons and adding them to 
+ *          ServicesData
  * 
  * Note: this file is going to be added more data
  * 
@@ -20,12 +24,60 @@ export const Aboutimg = [
     }
 ]
 
+/**brands icons */
 export const facebook = <FontAwesomeIcon icon={faFacebook} />
 export const instagram = <FontAwesomeIcon icon={faInstagram} />
 export const twitter = <FontAwesomeIcon icon={faTwitter} />
 export const linkedin = <FontAwesomeIcon icon={faLinkedin} />
 export const dribble = <FontAwesomeIcon icon={faDribbble} />
 export const pinterest = <FontAwesomeIcon icon={faPinterest} />
+
+/**services icons */
+export const brush = <FontAwesomeIcon icon={faPaintBrush}/> 
+export const  checklist = <FontAwesomeIcon icon={faListCheck}/>
+export const phonescreen = <FontAwesomeIcon icon={faMobileScreenButton}/>
+export const  paintrole = <FontAwesomeIcon icon={faPaintRoller}/>
+export const pensquare = <FontAwesomeIcon icon={faPenToSquare}/>
+export const  nodes = <FontAwesomeIcon icon={faShareNodes}/>
+
+export const ServicesData = [
+    {
+     id:1,
+     icon:brush,
+     name:'ui design',
+     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+     id:2,
+     icon:checklist,
+     name:'web development',
+     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+     id:3,
+     icon:phonescreen,
+     name:'app development',
+     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+     id:4,
+     icon:paintrole,
+     name:'type design',
+     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+     id:5,
+     icon:pensquare,
+     name:'blog writer',
+     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+     id:6,
+     icon:nodes,
+     name:'project management',
+     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+]
 
 export const SocialIcons = [
     {
