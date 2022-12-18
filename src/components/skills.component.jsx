d4365f56fb14135d84f8adf612c7aa1ae181aa61
Erick Rodriguez <erickrod54@gmail.com>
2022-12-18 18:17:27 -0500

    Scarlett-taylor Portfolio - version 2.01 - Skills - Features: Destructuring data from the context, Testing data

--- src/components/skills.component.jsx
+++ src/components/skills.component.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import { useAppContext } from "../context";
+
+/*Scarlett-taylor Portfolio - version 2.01 - 
+* Skills - Features: 
+*  
+*     --> Destructuring data from the context.
+*
+*     --> Testing data     
+*       
+* Note: this file will be refactor
+*/
 
 const Skills = () => {
 
+    const { SkillsData } = useAppContext()
+    console.log('testing skills data ==>', SkillsData)
+    console.log('testing skills data ==>', SkillsData[0].title)
+    console.log('testing skills data ==>', SkillsData[2].skillname)
+
     return(
-        <h2>Skills Component</h2>
+        <Wrapper>
+            <h2>Skills Component</h2>
+
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+
+`
 
 export default Skills;
