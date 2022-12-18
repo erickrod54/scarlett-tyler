ea26322b97076c61d7514d919a103879c135d5d7
Erick Rodriguez <erickrod54@gmail.com>
2022-12-18 18:15:58 -0500

    Scarlett-taylor Portfolio - version 2.01 - Services - Features: Writting styles, Writting media query rule for better responsiveness

--- src/components/services.component.jsx
+++ src/components/services.component.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import { useAppContext } from "../context";
 
-/*Scarlett-taylor Portfolio - version 2.00 - 
+/*Scarlett-taylor Portfolio - version 2.01 - 
 * Services - Features: 
 *  
-*     --> Adding icons    
+*     --> Writting styles.
+*
+*     --> Writting media query for
+*         better responsiveness.    
 *       
 * Note: this file will be refactor
 */
@@ -14,7 +17,7 @@ const Services = () => {
     
     const { ServicesData } = useAppContext()
 
-    console.log('the ServicesData =>',ServicesData)
+    //console.log('the ServicesData =>',ServicesData)
     
     return(
         <Wrapper>
@@ -24,8 +27,8 @@ const Services = () => {
                 return(
                     <div key={id} className="service">
                         <i>{icon}</i>
-                        <h3>{name}</h3>
-                        <p>{description}</p>
+                        <h3 className="service--title">{name}</h3>
+                        <p className="service--para">{description}</p>
                     </div>                  
                 )
             })}
@@ -34,7 +37,89 @@ const Services = () => {
 }
 
 const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-wrap: wrap;
 
+padding: 6rem 2rem 2rem;
+background-color: var(--primary-color);
+
+        .service{
+            padding: 4rem;
+            margin: 0 2rem 4rem;
+            flex-basis: 40rem;
+            flex-grow: 1;
+            background-color: var(--secondary-color);
+
+            /**this lines give style to the border */
+            border-bottom: 0.2rem solid var(--secondary-color);
+            border-right: 0.2rem solid var(--secondary-color);
+            border-left: 0.2rem solid var(--secondary-color);
+
+            /**to make the effect before the 'after' and 'hover' */
+            position: relative;
+
+            z-index: 1;
+
+            .service--title,
+            i{
+                font-size: 3rem;
+                margin-bottom: 2rem;
+                color: white;
+            }
+
+            .service--title{
+                text-transform: uppercase;
+            }
+
+            .service--para{
+                font-family: 'Indie Flower', cursive;
+                font-size: 2rem;
+                color: white;
+            }    
+        }
+
+        /**this style for the 'after effect' */
+        .service::after{
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 100%;
+            background-color: var(--primary-color);
+            transform: scaleX(0);
+            transition: transform 0.45s cubic-bezier(1, 0, 0, 1);
+
+            z-index: -1;
+        }
+
+        /**this effect for the 'hover' and 'active'
+        * using 'scaleX' will make the effect in X axis*/
+        .service:hover::after,
+        .service:active::after{
+            transform: scaleX(1);
+        }
+
+    /**media query rule to improve styles before 610px */    
+    @media screen and (max-width:610px){
+        padding: 6rem 3rem 2rem;
+
+        .service{
+            padding: calc(1rem + 2vw);
+            margin: 0 0 calc(1rem + 2vw);
+            
+            .service--title{
+                font-size: calc(2rem + 2vw);
+            }
+
+            .service--para{
+                font-size: calc(0.8rem + 1.5vw);
+            }
+        }
+
+    }
 `
 
 export default Services;
