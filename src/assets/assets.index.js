c773236c51aee86ccfd1d8b0a6d1612e9d9854c7
Erick Rodriguez <erickrod54@gmail.com>
2022-12-19 00:20:57 -0500

    Scarlett-taylor Portfolio - version 2.02 - assets.index - Features: Adding testimonionals assets

--- src/assets/assets.index.js
+++ src/assets/assets.index.js
@@ -6,15 +6,19 @@ import clothbag from './portfolio-creative-01-1.jpg';
 import branding from './portfolio-creative-02-1.jpg';
 import cup from './portfolio-design-01-1.jpg';
 import printer from './portfolio-print-02-1.jpg';
-import aboutimg from './content-img-02.jpg'
+import aboutimg from './content-img-02.jpg';
+import testimonial1 from './testimonials-1.jpg';
+import testimonial2 from './testimonials-2.jpg';
+import testimonial3 from './testimonials-3.jpg';
+import testimonial4 from './testimonials-4.jpg';
 
-/*Scarlett-taylor Portfolio - version 1.08 - 
+/*Scarlett-taylor Portfolio - version 2.02 - 
 * assets.index - Features: 
 *  
-*     --> Adding new assets 'aboutimg' component.
+*     --> Adding 'testimonials' assets.
 *  
 * Note: this file will be refactor
 */
 
 
-export { Logo, herofullimage, poster,paperbag, clothbag, branding, cup, printer, aboutimg } 
+export { Logo, herofullimage, poster,paperbag, clothbag, branding, cup, printer, aboutimg, testimonial1, testimonial2, testimonial3, testimonial4 } 
