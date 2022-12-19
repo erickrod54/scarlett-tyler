3028ec1a5ec98690bc95deed505964019c88ad4c
Erick Rodriguez <erickrod54@gmail.com>
2022-12-19 00:25:38 -0500

    Scarlett-taylor Portfolio - version 2.02 - Testimonials - Features: Destructuring data from the context, testing data

--- src/components/testimonials.component.jsx
+++ src/components/testimonials.component.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useAppContext } from "../context";
+
+/*Scarlett-taylor Portfolio - version 2.02 - 
+* Testimonials- Features: 
+*  
+*     --> Destructuring data from the context.
+*
+*     --> Testing data.       
+*       
+* Note: this file will be refactor
+*/
 
 const Testimonials = () => {
+
+    const { TestimonialsData } = useAppContext()
+    console.log('the testimonials  data ==>', TestimonialsData)
 
     return(
         <h2>Testimonials Component</h2>
