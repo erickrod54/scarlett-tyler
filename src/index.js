abf4207dd8483ccff9fd0a918f553f56c2679411
Erick Rodriguez <erickrod54@gmail.com>
2023-03-17 12:21:07 -0400

    Scarlett-taylor Portfolio - version 2.05 - index js file - Features: Wrapping the App with the provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import { AppProvider } from './context.js';
 
+/*Scarlett-taylor Portfolio - version 2.05 - 
+* index.js file - Features: 
+*  
+*     --> Wrapping the 'App' with the 
+*          provider
+*
+* Note: this file will be refactor
+*/
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
